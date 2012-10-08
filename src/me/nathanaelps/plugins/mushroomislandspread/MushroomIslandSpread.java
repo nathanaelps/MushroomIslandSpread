5a8f06f2dcf56b0b901fabffa8f450fa54b7d925
Nathanael Smith <nathanaelps@yahoo.com>
2012-10-08 15:55:16 -0500

    Removed ability for mushroom caps to grow on the ground, more or less. added red mushroom caps continued to add more rot-able blocks to the config.

--- src/me/nathanaelps/plugins/mushroomislandspread/MushroomIslandSpread.java
+++ src/me/nathanaelps/plugins/mushroomislandspread/MushroomIslandSpread.java
@@ -42,7 +42,7 @@ public class MushroomIslandSpread extends JavaPlugin implements Listener {
 
 		//registers this plugin
 		getServer().getPluginManager().registerEvents(this, this);
-
+		
 		//logs.... You know, you can figure this line out by yourself. Go for it.
 		log("Enabled.");
 	}
@@ -158,18 +158,23 @@ public class MushroomIslandSpread extends JavaPlugin implements Listener {
 		//Figure it out for yourself, or just accept it as magic. Either way.
 	}
 	
-	private void scheduleRot(final Block block) {
+	private void scheduleRot(Block block) {
+		scheduleRot(block, (int) (99+Math.round(Math.random())));
+		
+	}
+
+	private void scheduleRot(final Block block, final int capColor) {
 		//Remember how we checked to see if the nearBlock was a log? If it was, we send it down to here.
 		//Here we schedule it to actually rot sometime in the next 10 seconds.
 		//When it does rot, it'll use a slightly different function than the normal rot function.
 		this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 			public void run() {
-				rotLog(block);
+				rotLog(block, capColor);
 			}
 		}, (long) (Math.random()*200));
 	}
 
-	public void rotLog(Block block) {
+	public void rotLog(Block block, int capColor) {
 		//And this is the slightly different function that we use to rot logs.
 		//First, we'll pull the "I'm a Log!" list from the config again.
 		//I should probably streamline this so it doesn't have to check the config each time... Maybe?
@@ -179,7 +184,7 @@ public class MushroomIslandSpread extends JavaPlugin implements Listener {
 		if(!log.contains(block.getTypeId())) { return; }
 		
 		//Then we set it to Brown Mushroom Stem (block type id 99, block data 0x0)
-		block.setTypeId(99);
+		block.setTypeId(capColor);
 		
 		//Of course, if this block will spread its rot to another block, we won't consider it a cap.
 		boolean lastLeaf = true;
@@ -193,14 +198,13 @@ public class MushroomIslandSpread extends JavaPlugin implements Listener {
 					
 					//And we'll check to see if we can rot any farther.
 					//If we can, we schedule it (remember the previous function) and we'll remark that this isn't in fact the last leaf.
-					if(log.contains(nearBlock.getTypeId())) { scheduleRot(nearBlock); lastLeaf = false;}
+					if(log.contains(nearBlock.getTypeId())) { scheduleRot(nearBlock, capColor); lastLeaf = false;}
 				}
 			}
 		}
 		
-		//If it *is* the end of the line, we'll make it a mushroom cap. Which is currently just a brown mushroom block, singular.
-		//We should make it more dramatic... later.
-		if(lastLeaf) { block.setData((byte) 14); }
+		//If it *is* the end of the line, we'll make it a mushroom cap.
+		if(lastLeaf) { block.setData((byte) 14); cap(block, capColor); }
 		
 		//otherwise, it's just a stem.
 		else { block.setData((byte) 15); }
@@ -209,6 +213,61 @@ public class MushroomIslandSpread extends JavaPlugin implements Listener {
 		//Note that this time the boolean is TRUE, that's because we don't want the rotBlock function to deal with nearby logs,
 		//since we've already dealt with them in this function.
 		rotBlock(block,true);
+	}
+	
+	private void cap(Block block, int capColor) {
+		//cap will make a mushroom cap.	
+
+		List<Integer> offGround = this.getConfig().getIntegerList("skyBlocks");
+		
+		if(!offGround.contains(block.getRelative(0, -3, 0).getTypeId())) { return; }
+
+		//The maximum size of the cap.
+		double radius = 2.5;
+		
+		if(capColor == 99) {
+			//Same i and k as before. Brown caps are flat, so we don't have to think about j (or, y)
+			for(int i=-2; i<=2; i++){
+				for(int k=-2; k<=2; k++){
+					//This gives the cap a roundish shape.
+					if(Math.sqrt((i*i)+(k*k))>radius) { continue; }
+
+					//Again, we'll name the nearby block to nearBlock
+					Block nearBlock = block.getRelative(i,0,k);
+					//make it a mushroom block
+					nearBlock.setTypeId(capColor);
+					//that's a cap-style block.
+					nearBlock.setData((byte) 14);
+				}
+			}
+		} else if (capColor==100) {
+			//Same i, j, and k as before.
+			for(int i=-1; i<=1; i++){
+				for(int j=-1; j<=1; j++){
+					for(int k=-1; k<=1; k++){
+						if(j==1) {
+							if(Math.sqrt((i*i)+(k*k))>1) { continue; }
+							
+							//Again, we'll name the nearby block to nearBlock
+							Block nearBlock = block.getRelative(i,j,k);
+							//make it a mushroom block
+							nearBlock.setTypeId(capColor);
+							//that's a cap-style block.
+							nearBlock.setData((byte) 14);
+						} else {
+							if(Math.sqrt((i*i)+(k*k))<1){ continue; }
+							
+							//Again, we'll name the nearby block to nearBlock
+							Block nearBlock = block.getRelative(i,j,k);
+							//make it a mushroom block
+							nearBlock.setTypeId(capColor);
+							//that's a cap-style block.
+							nearBlock.setData((byte) 14);
+						}
+					}
+				}
+			}
+		}
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
